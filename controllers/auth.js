277f1b2cca004babaaa1ff79dcc49f22da6f9ea7
mauriciovaldora <mauricio_valdora@hotail.com.ar>
2023-07-01 19:59:45 -0300

    Actualizacion con google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const bcrypt = require('bcryptjs')
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generarjwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -44,6 +45,56 @@ const login = async (req, res = response) => {
 
 }
 
+
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { nombre, img, correo } = await googleVerify(id_token)
+        let usuario = await Usuario.findOne({ correo })
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({ msg: 'Usuario bloqueado' })
+        }
+
+        const token = await generarJWT(usuario.id);
+
+
+        res.json({
+            usuario,
+            token
+
+        })
+    }
+
+    catch (error) {
+        console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: 'token no se pudo verificar'
+        })
+    }
+
+}
+
+
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,21 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const {login} = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router()
 
 router.post('/login',
+    check('email', 'el correo es obligatorio').isEmail(),
+    check('password', 'el password es obligatorio').not().isEmpty(),
+    validarCampos, login)
 
-check('email', 'el correo es obligatorio').isEmail(),
-check('password', 'el password es obligatorio').not().isEmpty(),
-validarCampos
 
-,login )
+router.post('/google', [
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos],
+    googleSignIn)
 
 
 module.exports = router;
